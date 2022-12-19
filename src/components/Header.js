2b602c60eda2931bfdd2442f88615dc73119a862
leotricotti <tricottileo@gmail.com>
2022-12-19 16:46:10 -0300

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import logo from "../assets/img/logo/logo.webp";
 import NavToggle from "./NavToggle";
+import SayHelloBtn from "./SayHelloBtn";
 import styles from "../css/header.module.css";
+
 function Header() {
   return (
     <div className={styles.headerContainer}>
       <img src={logo} alt="Designer logo" className={styles.headerLogo} />
       <NavToggle />
+      <SayHelloBtn />
     </div>
   );
 }
