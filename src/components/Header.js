c656a06bc8c75dd5c6b4bbbda41f763d660af253
Leonardo Tricotti <tricottileo@gmail.com>
2022-12-19 11:23:05 -0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import logo from "../assets/img/logo/logo.webp";
+
+function Header() {
+  return (
+    <div className="header-container">
+      <img src={logo} alt="Designer logo" />
+    </div>
+  );
+}
