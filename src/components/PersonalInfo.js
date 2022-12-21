c16f258b446b0c80fb10605b4aeee7914655a8be
leotricotti <tricottileo@gmail.com>
2022-12-21 18:53:00 -0300

    commit

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -3,13 +3,13 @@ import styles from "../css/personalSection.module.css";
 function PersonalInfo() {
   return (
     <div className={styles.sectionContainer}>
-      <h2 className={styles.infoTitle}>
-        Hello, I´m Leonardo. Nice to meet you.
-      </h2>
+      <h2 className={styles.infoTitle}>Hello, nice to meet you.</h2>
       <p className={styles.personalInfo}>
-        Since I start to work as a frontend developer, I've done remote work for
-        agencies, consulted for startups, and collaborated with talented people
-        to create digital products for both business and consumer use.
+        Since beginning my journey as a frontend developer over 4 years ago,
+        I've done remote work for agencies, consulted for startups, and
+        collaborated with talented people to create digital products for both
+        business and consumer use. I'm quietly confident, naturally curious, and
+        perpetually working on improving my chops one design problem at a time.
       </p>
     </div>
   );
