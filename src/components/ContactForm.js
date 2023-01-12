ee7df67eb8f19c0c2b63ad5bb5394518634aff42
leotricotti <tricottileo@gmail.com>
2023-01-12 18:29:41 -0300

    first commit

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,5 +1,40 @@
-function Contact() {
-  return;
+import styles from "../css/contactForm.module.css";
+
+function ContactForm() {
+  return (
+    <section>
+      <form action="" className={styles.formContainer}>
+        <div className={styles.field}>
+          <label className={styles.formLabel}>Name</label>
+          <input
+            type="text"
+            name="name"
+            required
+            className={styles.formInput}
+          />
+        </div>
+        <div className={styles.field}>
+          <label className={styles.formLabel}>Email</label>
+          <input
+            type="email"
+            name="email"
+            required
+            className={styles.formInput}
+          />
+        </div>
+        <div className={styles.field}>
+          <label className={styles.formLabel}>Message</label>
+          <textarea
+            name="message"
+            rows={15}
+            required
+            className={styles.formInput}
+          />
+        </div>
+        <button className={styles.submit}>Submit</button>
+      </form>
+    </section>
+  );
 }
 
-export default Contact;
+export default ContactForm;
