3eb160ed2f277a0eef73bf9885f0a937b5ed2222
leotricotti <tricottileo@gmail.com>
2023-01-13 13:36:12 -0300

    commit

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,31 +1,56 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styles from "../css/contactForm.module.css";
 
 function ContactForm() {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  function handleInputChange(e) {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(
+      "enviando datos..." + data.name + " " + data.email + " " + data.message
+    );
+    e.target.reset();
+  }
+
   useEffect(() => {
     document.title = "Contact | Leonardo Tricotti";
   });
 
   return (
     <section>
-      <form action="" className={styles.formContainer}>
-        <div className={styles.field}>
-          <label className={styles.formLabel}>Name</label>
-          <input
-            type="text"
-            name="name"
-            required
-            className={styles.formInput}
-          />
-        </div>
-        <div className={styles.field}>
-          <label className={styles.formLabel}>Email</label>
-          <input
-            type="email"
-            name="email"
-            required
-            className={styles.formInput}
-          />
+      <form className={styles.formContainer} onSubmit={handleSubmit}>
+        <div className={styles.flex}>
+          <div className={styles.field}>
+            <label className={styles.formLabel}>Name</label>
+            <input
+              type="text"
+              name="name"
+              required
+              className={styles.formInput}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className={styles.field}>
+            <label className={styles.formLabel}>Email</label>
+            <input
+              type="email"
+              name="email"
+              required
+              className={styles.formInput}
+              onChange={handleInputChange}
+            />
+          </div>
         </div>
         <div className={styles.field}>
           <label className={styles.formLabel}>Message</label>
@@ -34,9 +59,12 @@ function ContactForm() {
             rows={15}
             required
             className={styles.formInput}
+            onChange={handleInputChange}
           />
         </div>
-        <button className={styles.submit}>Submit</button>
+        <button className={styles.submit} type="submit">
+          Submit
+        </button>
       </form>
     </section>
   );
