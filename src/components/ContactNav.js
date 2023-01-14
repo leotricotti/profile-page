cb9c44ffaa3e3f56d2eb8bd0bd3e903216b074dd
leotricotti <tricottileo@gmail.com>
2023-01-14 09:29:07 -0300

    commit

--- src/components/ContactNav.js
+++ src/components/ContactNav.js
@@ -1,11 +1,16 @@
 import logo from "../assets/img/logo/logo.webp";
+import undo from "../assets/img/icons/undo.png";
+import close from "../assets/img/icons/close.png";
 import styles from "../css/contactNav.module.css";
 
 function ContactNav() {
   return (
     <nav className={styles.navContainer}>
       <img src={logo} alt="Designer logo" className={styles.navLogo} />
-
+      <div className={styles.btnContainer}>
+        <img src={undo} alt="Undo button" className={styles.undoBtn} />
+        <img src={close} alt="Close button" className={styles.closeBtn} />
+      </div>
     </nav>
   );
 }
