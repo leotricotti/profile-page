be0d312f1175339fb1b74c7fa0878146baae0e71
leotricotti <tricottileo@gmail.com>
2023-05-21 14:47:38 -0300

    commit

--- src/components/contact/ContactHero.js
+++ src/components/contact/ContactHero.js
@@ -8,7 +8,7 @@ function ContactHero() {
 
   return data.flatMap((constant) =>
     constant.contact.flatMap((contact) =>
-      contact.form.map((item) => (
+      contact.hero.map((item) => (
         <section className={styles.heroContainer} key={item.id}>
           <img src={logo} alt="Designer avatar" className={styles.heroAvatar} />
           <h1 className={styles.heroTitle}>{item.title}</h1>
