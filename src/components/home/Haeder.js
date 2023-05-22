e4c50826c893642ccbf1f43ace9db25fb6046355
Leonardo Tricotti <tricottileo@gmail.com>
2023-05-22 12:29:46 -0300

    commit

--- src/components/home/Haeder.js
+++ src/components/home/Haeder.js
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import Logo from "./Logo";
 import styles from "./header.module.css";
 
 function Header() {
   const [toggle, setToggle] = useState(false);
+
+  useEffect(() => {
+    document.body.style.overflow = toggle ? "hidden" : "auto";
+  }, [toggle]);
+
   return (
     <header
       className={`${styles.headerContainer} ${toggle ? styles.isActive : ""}`}
